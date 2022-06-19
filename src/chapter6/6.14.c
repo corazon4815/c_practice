303e8d99675fafb17274d07f5a0a4a3c94395b84
corazon4815 <mango4815@gmail.com>
2022-06-19 05:48:45 +0000

    6-14

--- src/chapter6/6.14.c
+++ src/chapter6/6.14.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void main() 
 {
-    float array[]={100.0, 200.0, 300.0, 400.0, 500.0};
-    float *ptr;
-    int len;
-    ptr=&array[0]; //포인터를 배열의 첫번째 요소로 지정
-    printf("%.1f\n", *ptr); //100.0을 출력
-    ptr++;
-    printf("%.1f\n", *ptr); //200.0을 출력
-    ptr++;
-    printf("%.1f\n", *ptr); //300.0을 출력
-    ptr++;
-    printf("%.1f\n", *ptr); //400.0을 출력
-    ptr++;
-    printf("%.1f\n", *ptr); //500.0을 출력
+    #define TOTAL 5
+    char *song[10];
+    char newSong[80];
+    int i;
+    for(i=0; i<TOTAL; i++){
+        printf("애창곡 #%d?", (i+1));
+        gets(newSong);
+        song[i]=(char *) malloc((strlen(newSong)+1) * sizeof(char));
+        strcpy(song[i], newSong);
+    }
+    printf("***입력한 노래 제목***\n");
+    for(i=(TOTAL-1); i>=0; i--){
+        printf("노래 #%d: %s\n", (i+1), song[i]);
+        free(song[i]); //힙 공간을 모두 돌려줘야 함
+    }
     
-    ptr=array; //포인터의 위치를 처음 위치로 지정
     
-    printf("%.1f\n", *(ptr+2)); //300.0을 출력하지만 포인터의 위치는 바뀌지 않음
-    
-    printf("%.1f %.1f\n", (ptr+0)[0], (array+0)[0]); //100.0 100.0
-    printf("%.1f %.1f\n", (ptr+1)[0], (array+1)[0]); //200.0 200.0
-    printf("%.1f %.1f\n", (ptr+4)[0], (array+4)[0]); //500.0 500.0
-    printf("%.1f %.1f\n", (ptr-1)[2], (array-1)[2]); //200.0 200.0
-    printf("%.1f %.1f\n", (ptr-20)[23], (array-20)[23]); //400.0 400.0
-    return;
 }
